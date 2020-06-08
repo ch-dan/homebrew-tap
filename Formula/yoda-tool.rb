6357b2a5fb5f55e514a05a010cae7b7610156abd
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-08 07:21:28 +0100

    Brew formula update for yoda version v1.0.14

--- Formula/yoda-tool.rb
+++ Formula/yoda-tool.rb
@@ -3,16 +3,16 @@ require_relative "../custom_download_strategy"
 class YodaTool < Formula
   desc ""
   homepage ""
-  version "1.0.13"
+  version "1.0.14"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ch-dan/yoda/releases/download/v1.0.13/yoda_1.0.13_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "33e3340817f9f955ec1297e3add19a70ec4d6c8d69ac486e1ace925ac67d5fe7"
+    url "https://github.com/ch-dan/yoda/releases/download/v1.0.14/yoda_1.0.14_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "df627077951a7070d35772afa385a25645c210382b4c675351b590c4c82da8a3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ch-dan/yoda/releases/download/v1.0.13/yoda_1.0.13_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "74e5554d0d8e027ae0e8b5939fd9ade583de19830d302d07ec11b1f5c55610cb"
+      url "https://github.com/ch-dan/yoda/releases/download/v1.0.14/yoda_1.0.14_Linux_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "16078a8891f5fa4a30e7a0484911511b697e0a5d35100916827692a341091042"
     end
   end
 
